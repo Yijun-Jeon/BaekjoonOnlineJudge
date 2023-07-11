f2a4225b403f936d70baf7258739983cfa877ec4
Yijun Jeon <gkqls813@gmail.com>
2023-07-11 23:05:25 +0900

    [Silver Ⅱ] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 88 ms, Memory: 12588 KB -BakjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 - 깊이 우선 탐색 2/알고리즘 수업 - 깊이 우선 탐색 2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define MAX_N 100000
+using namespace std;
+
+bool visited[MAX_N+1];
+int orders[MAX_N+1];
+int order = 1;
+
+void dsf(vector<int> graph[], int here){
+    visited[here] = true;
+    orders[here] = order++;
+    for(int i=0;i<graph[here].size();i++){
+        int next = graph[here][i];
+        if(visited[next] == false)
+            dsf(graph,next);
+    }
+}
+
+int main(void)
+{
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    int N,M,R;
+    cin >> N >> M >> R;
+
+    vector<int> graph[N+1];
+
+    for(int i=0;i<M;i++){
+        int u,v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    for(int i=1;i<=N;i++)
+        sort(graph[i].begin(),graph[i].end(),greater<int>());
+
+    dsf(graph,R);
+    for(int i=1;i<=N;i++)
+        cout << orders[i] << '\n';
+    return 0;
+}
